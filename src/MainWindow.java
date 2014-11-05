08750e14ac78fd9c3a0ffaa00dc63a5e24702040
Espen Moe <espen357moe@gmail.com>
2014-11-05 17:35:18 +0100

    Initialized MainWindow

---
+++ src/MainWindow.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class MainWindow extends JFrame {
+
+    public MainWindow(){
+        setTitle("SoundSergeant");
+        setSize(400,400);
+        setLayout(new BorderLayout());
+        setVisible(true);
+
+    }
+
+}
