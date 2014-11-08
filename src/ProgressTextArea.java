32d8530ac862af00a3f4f567afb4de9be21722db
Espen Moe <espen357moe@gmail.com>
2014-11-08 17:10:27 +0100

    Created a slightly styled text area for process progress status.

--- src/ProgressTextArea.java
+++ src/ProgressTextArea.java
@@ -1,9 +1,13 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class ProgressTextArea extends JTextArea {
 
     public ProgressTextArea() {
         this.setEditable(false);
+        this.setFont(new Font("Monospaced", Font.PLAIN, 12));
+        this.setBackground(Color.BLACK);
+        this.setForeground(Color.ORANGE);
     }
 
 }
