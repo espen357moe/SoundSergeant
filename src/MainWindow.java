9e5dadb7501e291a24b59d958c315938154bc94b
Espen Moe <espen357moe@gmail.com>
2014-11-08 17:08:59 +0100

    Added folder buttons, progress information area, convert button, and menubar

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,7 +1,9 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class MainWindow extends JFrame {
+public class MainWindow extends JFrame implements ActionListener{
 
     public MainWindow(){
         setTitle("SoundSergeant");
@@ -10,11 +12,14 @@ public class MainWindow extends JFrame {
         JMenuBar menubar = new JMenuBar();
         JMenu menu = new JMenu("Menu");
         JMenuItem m1 = new JMenuItem("About");
-        JMenuItem m2 = new JMenuItem("Close");
+        JMenuItem m2 = new JMenuItem("Exit");
 
         menubar.add(menu);
         menu.add(m1);
         menu.add(m2);
+
+        m1.addActionListener(this);
+        m2.addActionListener(this);
 
         setJMenuBar(menubar);
 
@@ -26,20 +31,32 @@ public class MainWindow extends JFrame {
 
         JButton sourceFolderButton = new JButton("Select source folder");
         JButton destinationFolderButton = new JButton("Select destination folder");
-        folderButtonsPanel.add(sourceFolderButton);
-        folderButtonsPanel.add(destinationFolderButton);
-
         JButton convertButton = new JButton("Convert");
 
         ProgressTextArea progressTextArea = new ProgressTextArea();
         progressTextArea.append("Idle...");
 
+        folderButtonsPanel.add(sourceFolderButton);
+        folderButtonsPanel.add(destinationFolderButton);
+
         mainContainer.add(folderButtonsPanel, BorderLayout.NORTH);
         mainContainer.add(progressTextArea, BorderLayout.CENTER);
         mainContainer.add(convertButton, BorderLayout.SOUTH);
 
-
         setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String event = e.getActionCommand();
+
+        if(event.equals("About")) {
+            new AboutWindow();
+        }
+
+        if(event.equals("Exit")) {
+            System.exit(0);
+        }
     }
 
 }
