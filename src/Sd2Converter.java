a5599725bdfddb28a9d5784c86cd1754f39ad8a9
Espen Moe <espen357moe@gmail.com>
2015-07-10 13:29:56 +0200

    La til grunnleggende stæsj for konverteringsklassen

---
+++ src/Sd2Converter.java
@@ -0,0 +1,18 @@
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class Sd2Converter {
+
+    private final InputStream inputStream;
+    private final OutputStream outputStream;
+
+    public Sd2Converter(InputStream inputStream, OutputStream outputStream) {
+        this.inputStream = inputStream;
+        this.outputStream = outputStream;
+
+    }
+
+    public void convert(){
+        inputStream.
+    }
+}
